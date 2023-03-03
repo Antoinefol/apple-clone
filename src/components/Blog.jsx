98732a483a85fe7f179d3f3ff14ac6d742f949b0
Antoinefol <antfoliard@gmail.com>
2023-03-03 13:56:43 +0100

    Added text on images

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -6,12 +6,11 @@ import { LittleImages } from "./LittleImages"
 export function Blog() {
     return (
       <div className="blog-wrap">
-        <BigImage imageSrc="iphone14-pro.jpg"/>
-        <BigImage imageSrc="iphone14-tall.jpg"/>
-        <BigImage imageSrc="apple-tall.jpg"/>
-        <LittleImages imageSrc="promo.jpg"/>
-        <LittleImages imageSrc="ipad-cover.jpg"/>
-        
+        <BigImage logo="" textColor="white" title="iPhone 14 Pro" catchphrase="Pro. Plus Ultra" BuyLink="a" MoreLink="a" imageSrc="iphone14-pro.jpg"/>
+        <BigImage logo="" textColor="" title="iPhone 14" catchphrase="Un grand. Avec un grand Plus." BuyLink="" MoreLink="" imageSrc="iphone14-tall.jpg"/>
+        <BigImage logo="logo-watch.png" textColor="white" title="" catchphrase="Plus en forme que jamais." BuyLink="" MoreLink="" imageSrc="apple-tall.jpg"/>
+        <LittleImages logo="" textColor="" title="iPad" catchphrase="Vos idées. Sa magie. Du génie" BuyLink="" MoreLink="" imageSrc="ipad-cover.jpg"/>
+        <LittleImages logo="" textColor="white" title="MacBook Pro" catchphrase="Boosté par les puces M2 Pro et M2 Max." BuyLink="" MoreLink="" imageSrc="promo.jpg"/>
         <HomeButton/>
       </div>
     );
